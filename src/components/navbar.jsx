cc7fb40a36437962209296c447482045a6f8c75e
BoneNzy <geremsanrzr@gmail.com>
2023-06-26 01:02:24 +0530

    Toggle theme

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,7 +11,13 @@ import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "../state/state.jsx";
 import profilePicture from "../assets/images/MyPfp2.jpg";
-import { AppBar, Toolbar, useTheme } from "@mui/material";
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -31,6 +37,35 @@ const Navbar = () => {
         }}
       >
         {/* Left Side */}
+        <FlexBetween>
+          <IconButton onClick={() => console.log("Toggle sidebar")}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="8px"
+            gap="3rem"
+            p="0.1rem 0.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+        {/* RIght Side */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton onClick={() => console.log("Settings toggle")}>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </FlexBetween>
       </Toolbar>
     </AppBar>
   );
